79b3854ecfe7a24d837b78e6b8a7a2f791e1c84e
nguyentrinhduy <duy.nguyentrinh@sun.studio>
2024-09-06 22:56:57 +0700

    add following and testing toggler to the camera debugger

--- Assets/Scripts/CameraTest.cs
+++ Assets/Scripts/CameraTest.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class CameraTest : MonoBehaviour
 {
     public GameObject player;
+
+    public bool isTesting = true;
+    public bool isFollowing = true;
     public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset = new Vector3(0, 4, -10);
     }
 
+    public void ToggleFollowing() {
+        isFollowing = !isFollowing;
+        if (isFollowing) {
+            offset = new Vector3(0, 4, -10);
+            transform.rotation = Quaternion.Euler(30, 0, 0);
+        }
+    }
     // Update is called once per frame
     void Update()
-    {
-        transform.position = player.transform.position + offset;
+    {   
+        if (isTesting) {
+            if (isFollowing) {
+                transform.position = player.transform.position + offset;
+            }
+        }
+        
     }
 }
