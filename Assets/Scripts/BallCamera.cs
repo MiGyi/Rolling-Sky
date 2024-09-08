8c69e89cf4fab316cbb56e7fba2c78b75ff59daa
Giang Phạm <pvmgiang21@apcs.fitus.edu.vn>
2024-09-08 23:35:14 +0700

    Camera for Ball

---
+++ Assets/Scripts/BallCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BallCamera : MonoBehaviour
+{
+    public GameObject Ball;
+    public Vector3 offset = new Vector3(0, 4, -6);
+    public float CameraVerticalRotation = 30;
+    public float MaximumHorizontalCameraRange = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(CameraVerticalRotation, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yPos = Ball.transform.position.y;
+        float zPos = Ball.transform.position.z;
+        float xPos = Ball.transform.position.x / 2f;
+
+        if (xPos < 0) xPos = Mathf.Max(xPos, -MaximumHorizontalCameraRange);
+        else xPos = Mathf.Min(xPos, MaximumHorizontalCameraRange);
+
+        offset.x = xPos;
+
+        transform.position = new Vector3(0, yPos, zPos) + offset;
+    }
+}
