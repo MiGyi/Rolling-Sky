5779d3cbe31e455fd48bd98e70aef5253db20835
Văn Phúc Lưu <hoktro18@gmail.com>
2024-09-08 16:05:07 +0700

    fix: adapt mouse tracking code to general code ( also create demo )

--- Assets/Scripts/Input Controller/MouseTracking.cs
+++ Assets/Scripts/Input Controller/MouseTracking.cs
@@ -14,13 +14,25 @@ public class MouseTracking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (mainCamera != null)
+        //{
+        //    Vector3 mousePosition = Input.mousePosition;
+        //    mousePosition.z = depthValue;
+        //    Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+        //    transform.position = worldPosition;
+        //    Debug.Log("Mouse Position: " + worldPosition);
+        //}
+    }
+
+    private void FixedUpdate()
+    {
         if (mainCamera != null)
         {
             Vector3 mousePosition = Input.mousePosition;
             mousePosition.z = depthValue;
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
             transform.position = worldPosition;
-            Debug.Log("Mouse Position: " + worldPosition);
+            //Debug.Log("Mouse Position: " + worldPosition);
         }
     }
 }
