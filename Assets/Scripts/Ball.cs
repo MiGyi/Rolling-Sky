6e86d06783a9424c872b40a9df2f51559a74c318
Văn Phúc Lưu <hoktro18@gmail.com>
2024-09-13 20:40:55 +0700

    feat: ball movement
    - Auto run forward
    - If the ball drop, disabled tracking mouse and running
    - Attach mouse tracking to ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,33 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Ball : MonoBehaviour
 {
     public Rigidbody rb;
-    public float speed = 10.0f;
-    public float jumpingForce = 5.0f;
-    // Start is called before the first frame update
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jumpingForce = 5.0f;
+    [SerializeField] private float deltaSpeed = 0.05f;
+
+    private bool isPlayable = false;
+
+    private KeyboardController keyboardController = null;
+    private MouseTracking mouseTracking = null;
+
+    void Awake()
+    {
+        GameObject gameManager = GameObject.FindGameObjectWithTag("GameManager");
+
+        // check if GameManager is found    
+        if( gameManager != null )
+        {
+            keyboardController = gameManager.GetComponent<KeyboardController>();
+            mouseTracking = gameManager.GetComponent<MouseTracking>();
+
+            // check if KeyboardController is found
+            if( keyboardController == null )
+            {
+                Debug.LogError("KeyboardController not found");
+            }
+
+            // check if MouseTracking is found
+            if( mouseTracking == null )
+            {
+                Debug.LogError("MouseTracking not found");
+            }
+        }
+        else {
+            Debug.LogError("GameManager not found");
+        }
+    }
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        isPlayable = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float Horizontal = Input.GetAxis("Horizontal");
-        float Vertical = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(Horizontal, 0, Vertical);
-        Vector3 velocity = rb.velocity;
-        velocity.x = direction.x * speed;
-        velocity.z = direction.z * speed;
-        rb.velocity = velocity;
+        if( keyboardController == null || mouseTracking == null ) return;
+        if (!keyboardController.inGame || !isPlayable) return;                                                         // if not in game, abort
 
-        // press space to jump
-        if (Input.GetButtonDown("Jump"))
+        // Move the ball forward
+        AutoMoveForward();
+    }
+
+    void FixedUpdate()
+    {
+        if( transform.position.y < 0.3f )
         {
-            rb.AddForce(Vector3.up * jumpingForce, ForceMode.Impulse);
+            isPlayable = false;
         }
+
+        if (keyboardController == null || mouseTracking == null) return;
+        if (!keyboardController.inGame || !isPlayable) return;                                                         // if not in game, abort
+
+        // Move the ball to the mouse position
+        MouseFollowed();
+    }
+
+    private void MouseFollowed()
+    {
+        if( keyboardController == null || mouseTracking == null ) return;
+        if (!keyboardController.inGame || !isPlayable) return;                                                         // if not in game, abort
+
+        float xValue = mouseTracking.getXValue();                                                       // get the z value from the mouse position
+        transform.position = new(xValue, transform.position.y, transform.position.z);                   // move the ball to the target position
+    }
+
+    private void AutoMoveForward()
+    {
+        if( keyboardController == null ) return;
+        if (!keyboardController.inGame || !isPlayable) return;                                                         // if not in game, abort
+
+        // Auto run forward with speed increase over time
+        rb.velocity = new Vector3(0, 0, speed);
+        speed += Time.deltaTime * deltaSpeed;
     }
 }
